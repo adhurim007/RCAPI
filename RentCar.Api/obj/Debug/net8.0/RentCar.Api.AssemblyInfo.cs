a1d2703ed02fd921729c75540fbb79732fd52b68
adhurim-local <adhurim077@gmail.com>
2025-08-15 16:11:01 +0200

    ..

--- RentCar.Api/obj/Debug/net8.0/RentCar.Api.AssemblyInfo.cs
+++ RentCar.Api/obj/Debug/net8.0/RentCar.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentCar.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0530fd053d7cbdeb2b5521603940ced071278389")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentCar.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentCar.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
