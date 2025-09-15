0554d6166a9702967e1c7d99fb644035ea5b1047
adhurim-local <adhurim077@gmail.com>
2025-09-15 16:50:17 +0200

    //

--- RentCar.Infrastructure/obj/Debug/net8.0/RentCar.Infrastructure.AssemblyInfo.cs
+++ RentCar.Infrastructure/obj/Debug/net8.0/RentCar.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentCar.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18c41aee2bdf39a58e443e01b0d00b5745bddeb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3106f597a7f8d2345d460e4b68e0877fe68268d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentCar.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentCar.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
