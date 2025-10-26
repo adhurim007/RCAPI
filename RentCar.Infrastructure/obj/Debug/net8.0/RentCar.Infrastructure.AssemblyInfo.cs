036b7c93a7dcd6c8074c92ca326073ca20df905e
adhurim-local <adhurim077@gmail.com>
2025-10-26 21:56:45 +0100

    // Update Rent a Car System Managment modules

--- RentCar.Infrastructure/obj/Debug/net8.0/RentCar.Infrastructure.AssemblyInfo.cs
+++ RentCar.Infrastructure/obj/Debug/net8.0/RentCar.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentCar.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86fc14c597eeafcc58a84ba59fd4d0ad60de1ac1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87a01feeefae480a7cf461a2437d1c5f3da7ffe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentCar.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentCar.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
